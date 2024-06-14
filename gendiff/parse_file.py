3189ad57fdc5733cc443538693432eb509a0af93
Evgeny <info@tinamagic.lv>
2024-06-14 13:43:53 +0300

    Step 8

--- gendiff/parse_file.py
+++ gendiff/parse_file.py
@@ -7,14 +7,15 @@ from pathlib import Path
 def get_dict_from_file(path_file):
     file_ext = Path(path_file).suffix
     path_file = Path() / 'tests/fixtures' / os.path.basename(path_file)
-    f = open(path_file)
-    return open_file(f, file_ext)
+    with open(path_file) as f:
+        s = f.read()
+    return open_file(s, file_ext)
 
 
-def open_file(f, file_ext):
+def open_file(s, file_ext):
     if file_ext.lower() == '.json':
-        return json.load(f)
+        return json.loads(s)
     elif file_ext.lower() == '.yml' or file_ext.lower() == '.yaml':
-        return yaml.safe_load(f)
+        return yaml.safe_load(s)
     else:
         raise ValueError('This file type is not supported!')
